c9f2eaadb8c736a2d848892739e600aacf72eb23
Lyndon <xianzhiwu@trip.com>
2024-08-19 10:17:17 +0800

    feat: description & env enum

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,4 +1,4 @@
-export enum EnvEnum {
+export enum Env {
   Fat = "fat",
   Prod = "prod",
 }
--- src/utils/get-request-base-url.ts
+++ src/utils/get-request-base-url.ts
@@ -1,20 +1,20 @@
-import { EnvEnum } from "@/constants/env";
+import { Env } from "@/constants/env";
 
 /**
  * 通过 hostname 来判断接口调用环境
  */
 export const getRequestBaseUrl = (soaCode: string) => {
   // TODO: 修改为自己项目的域名
-  const formatEnv = window?.location?.hostname === "xxx.xxx.com" ? EnvEnum.Prod : EnvEnum.Fat;
+  const formatEnv = window?.location?.hostname === "xxx.xxx.com" ? Env.Prod : Env.Fat;
 
   const soaApiPrefix = {
-    [EnvEnum.Fat]:
+    [Env.Fat]:
       typeof window === "undefined"
         ? `https://xxx.xxx.com/restapi/soa2`
         : // 本地域名默认 http，只能走代理，不能直接使用 localhost
           `http://xxx.xxx.com/soa2`,
 
-    [EnvEnum.Prod]: typeof window === "undefined" ? `https://xxx.xxx.com/restapi/soa2` : `https://xxx.xxx.com/soa2`,
+    [Env.Prod]: typeof window === "undefined" ? `https://xxx.xxx.com/restapi/soa2` : `https://xxx.xxx.com/soa2`,
   };
 
   return `${soaApiPrefix[formatEnv]}/${soaCode}`;
